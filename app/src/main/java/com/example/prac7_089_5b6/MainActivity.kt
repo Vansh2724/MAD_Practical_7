7e908311062c9d96e1f2e6c94f5e9d0a27cfcf48
Vansh <vanshved2022@gmail.com>
2023-09-22 12:39:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prac7_089_5b6/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.prac7_089_5b6
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import androidx.core.view.WindowCompat
+import com.example.prac7_089_5b6.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initVideoPlayer()
+        binding.btnSwitch.setOnClickListener{
+           Intent(this,YoutubeActivity::class.java).also{ startActivity(it)}
+            }
+    }
+
+
+private fun initVideoPlayer()
+{
+    val mediaController = MediaController(this)
+    val uri: Uri = Uri.parse("android.resource://"+ packageName + "/" + R.raw.thestoryoflight)
+    binding.myVideoview.setMediaController(mediaController)
+    mediaController.setAnchorView(binding.myVideoview)
+    binding.myVideoview.setVideoURI(uri)
+    binding.myVideoview.requestFocus()
+    binding.myVideoview.start()
+}
+}
---
+++ app/src/main/java/com/example/prac7_089_5b6/YoutubeActivity.kt
@@ -0,0 +1,36 @@
+package com.example.prac7_089_5b6
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebSettings
+import androidx.core.view.WindowCompat
+import com.example.prac7_089_5b6.databinding.ActivityYoutubeBinding
+
+class YoutubeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityYoutubeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityYoutubeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initYoutbePlayer()
+        binding.btnSwitch.setOnClickListener(){
+            Intent(this,MainActivity::class.java).also { startActivity(it) }
+        }
+
+    }
+
+    private val youtubeId = "fJn9B64Znrk"
+
+    private fun initYoutbePlayer()
+    {
+        val webSettings: WebSettings = binding.youtubeWebview.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+        binding.youtubeWebview.loadUrl("https://www.youtube.com/embed/$youtubeId")
+    }
+}
